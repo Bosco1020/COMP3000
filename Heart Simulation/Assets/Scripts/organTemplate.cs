e0e71ee0e5368f2a023cac7491819d6f10abd271
Connor Boscherini <connorbos@hotmail.co.uk>
2022-02-17 00:57:53 +0000

    2nd View & fading
    Copied view to create a 2nd one for testing.
    
    Continued attempting to fade out heart. Seemingly works but the model doesn't update until the shader is selected in inspector. Used material Lerp and directly editing material properties

--- Heart Simulation/Assets/Scripts/organTemplate.cs
+++ Heart Simulation/Assets/Scripts/organTemplate.cs
@@ -19,20 +19,22 @@ public class organTemplate : MonoBehaviour
 
     public Animator IdleAnim;
 
-    public Material[] heartMats;
+    public Renderer[] heartMats;
+
+    //public Material FadeMat;
 
     [Range (0, 10)]
     public float idleRate = 1.0f;
 
     public bool speedOveride = false;
 
-    [SerializeField, Range(0,1)]
-    private float alpha = 1;
+    //private bool fadingOut = false;
 
     void Start()
     {
         currentView = -1;
         IdleAnim.speed = idleRate;
+        //fadeOut();
     }
 
     void Update()
@@ -42,10 +44,15 @@ public class organTemplate : MonoBehaviour
             IdleAnim.speed = idleRate;
         }
 
-        foreach (Renderer mat in this.gameObject.GetComponentsInChildren<Renderer>())
+        
+        /*
+        if(fadingOut)
         {
-            mat.material.color.a.Equals(Mathf.Lerp(1, 0, 0.1f * Time.deltaTime));
-        }
+            foreach (Renderer mat in heartMats)
+            {
+                mat.material.Lerp(mat.material, FadeMat, 1.0f * Time.deltaTime);
+            }  
+        }*/
     }
 
     public List<coOrdinateSystem> returnActiveCells()
@@ -76,30 +83,36 @@ public class organTemplate : MonoBehaviour
 
     public void fadeOut()
     {
-        foreach (Renderer mat in this.gameObject.GetComponentsInChildren<Renderer>())
+        foreach (Renderer mat in heartMats)
         {
+            
             mat.material.SetFloat("_Mode", 2);
             mat.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
             mat.material.SetInt("_DstBelnd", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-            mat.material.SetInt("_ZWrite", 0);
+            //mat.material.SetInt("_ZWrite", 0);
             mat.material.DisableKeyword("_ALPHATEST_ON");
             mat.material.EnableKeyword("_ALPHABLEBD_ON");
             mat.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
             mat.material.renderQueue = 3000;
+            
         }
+        //fadingOut = true;
+        StartCoroutine(fadeOutCoroutine());
     }
 
     public void fadeIn()
     {
-        foreach (Renderer mat in this.gameObject.GetComponentsInChildren<Renderer>())
+        foreach (Renderer mat in heartMats)
         {
             mat.material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
             mat.material.SetInt("_DstBelnd", (int)UnityEngine.Rendering.BlendMode.Zero);
-            mat.material.SetInt("_ZWrite", 1);
+            //mat.material.SetInt("_ZWrite", 1);
             mat.material.DisableKeyword("_ALPHATEST_ON");
             mat.material.DisableKeyword("_ALPHABLEBD_ON");
             mat.material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
             mat.material.renderQueue = 1;
+            mat.material.EnableKeyword("_NORMALMAP");
+            mat.material.EnableKeyword("_DETAIL_MULX2");
         }
     }
 
@@ -121,11 +134,24 @@ public class organTemplate : MonoBehaviour
         }
     }
 
-    IEnumerator fadeCoroutine(float change)
+    IEnumerator fadeOutCoroutine()
     {
-        while (true)
-        {
+        float count = 1.0f;
+        Color c;
 
+        while (count > 0.8f)
+        {
+            foreach (Renderer mat in heartMats)
+            {
+                c = mat.material.color;
+                c.a = count;
+                mat.material.SetColor("_Color", c);
+                Debug.Log(mat.material.color.a);
+            }
+            count = count - 0.01f;
+            Debug.Log("-----");
         }
+
+        yield return new WaitForSeconds(0.1f);
     }
 }
